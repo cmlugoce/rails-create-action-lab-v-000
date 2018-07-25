6a49e97addc678f41e68d50b1a3ecada2e4e971c
Cristina Lugo <cmlugoce@syr.edu>
2018-07-25 21:30:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,13 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new
-    @student = Student.new
-  end
-
   def create
-    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to_student_path(@student)
-  end
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+   end
 
 end
